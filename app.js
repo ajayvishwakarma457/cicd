fdd533587fb4c1333adb081bbe9e920764b57d3a
Ajay <ajayvishwakarma457@gmail.com>
2025-06-04 21:21:58 +0530

    changes

---
+++ app.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Example POST route
+app.post('/data', (req, res) => {
+  const received = req.body;
+  res.json({ message: 'Data received', data: received });
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
